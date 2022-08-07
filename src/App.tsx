a8de86865af9f899833e3bfee2376acedad8fe63
Cupid Valentine <westerngod@126.com>
2022-08-07 16:43:30 +0800

    feat: init css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { defineComponent } from 'vue'
+import { RouterView } from 'vue-router'
+import './App.scss'
+
 export const App = defineComponent({
   setup() {
-    return () => <div></div>
+    return () => <RouterView />
   }
 })
