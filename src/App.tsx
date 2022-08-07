0bf2e4852249473eb5dd2e15ae38dcd3c3fe9cf0
Cupid Valentine <westerngod@126.com>
2022-08-07 15:26:14 +0800

    refactor: optimize routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-import { defineComponent, ref } from 'vue'
-
+import { defineComponent } from 'vue'
 export const App = defineComponent({
   setup() {
-    const count = ref(0)
-    const onClick = () => {
-      count.value += 1
-    }
-    return () => (
-      <>
-        <div>{count.value}</div>
-
-        <div>
-          <button onClick={onClick}>+1</button>
-        </div>
-      </>
-    )
+    return () => <div></div>
   }
 })
