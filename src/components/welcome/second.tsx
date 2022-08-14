3915af34c2a56007f71186fcf7083b7c875d10d9
Cupid Valentine <westerngod@126.com>
2022-08-14 18:03:09 +0800

    refactor: simplified the code of welcome page

--- src/components/welcome/second.tsx
+++ src/components/welcome/second.tsx
@@ -1,5 +1,3 @@
-import { defineComponent } from 'vue'
-
 import s from './WelcomeLayout.module.scss'
 
 import clock from '../../assets/icons/clock.svg'
@@ -8,30 +6,28 @@ import { RouterLink } from 'vue-router'
 
 import { WelcomeLayout } from './WelcomeLayout'
 
-export const Second = defineComponent({
-  setup() {
-    return () => (
-      <WelcomeLayout>
-        {{
-          icon: <img src={clock} alt="" />,
-          title: () => (
-            <h2>
-              每日提醒 <br /> 不遗漏每一笔账单
-            </h2>
-          ),
-          buttons: () => (
-            <>
-              <RouterLink class={s.fake} to="/start">
-                跳过
-              </RouterLink>
-              <RouterLink class={s.next} to="/welcome/3">
-                下一页
-              </RouterLink>
-              <RouterLink to="/start">跳过</RouterLink>
-            </>
-          )
-        }}
-      </WelcomeLayout>
-    )
-  }
-})
+export const Second = () => (
+  <WelcomeLayout>
+    {{
+      icon: <img src={clock} alt="" />,
+      title: () => (
+        <h2>
+          每日提醒 <br /> 不遗漏每一笔账单
+        </h2>
+      ),
+      buttons: () => (
+        <>
+          <RouterLink class={s.fake} to="/start">
+            跳过
+          </RouterLink>
+          <RouterLink class={s.next} to="/welcome/3">
+            下一页
+          </RouterLink>
+          <RouterLink to="/start">跳过</RouterLink>
+        </>
+      )
+    }}
+  </WelcomeLayout>
+)
+
+Second.displayName = 'Second'
