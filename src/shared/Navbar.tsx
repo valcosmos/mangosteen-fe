8178caad2f1cda7dd27e886fc2e7e2fd62ef310b
Cupid Valentine <westerngod@126.com>
2022-08-20 15:34:09 +0800

    feat: add navbar component

---
+++ src/shared/Navbar.tsx
@@ -0,0 +1,13 @@
+import { defineComponent } from 'vue'
+import s from './Navbar.module.scss'
+
+export const Navbar = defineComponent({
+  setup(props, { slots }) {
+    return () => (
+      <div class={s.navbar}>
+        <span class={s.icon_wrapper}>{slots.icon?.()}</span>
+        <span class={s.title_wrapper}>{slots.default?.()}</span>
+      </div>
+    )
+  }
+})
