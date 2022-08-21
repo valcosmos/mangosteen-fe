168cc88be5dd26f5a6fb3f0e03a1c02dd94b6147
Cupid Valentine <westerngod@126.com>
2022-08-21 18:19:19 +0800

    refactor: work in progress with tabs component

--- src/components/Item/ItemCreate.tsx
+++ src/components/Item/ItemCreate.tsx
@@ -1,19 +1,32 @@
 import { MainLayout } from '@/Layouts/MainLayout'
 import { Icon } from '@/shared/Icon'
-import { defineComponent } from 'vue'
+import { Tab, Tabs } from '@/shared/Tabs'
+import { defineComponent, ref } from 'vue'
 
 import s from './ItemCreate.module.scss'
 
 export const ItemCreate = defineComponent({
   setup() {
-    console.log(111)
+    const refKind = ref('支出')
 
     return () => (
       <div class={s.wrapper}>
         <MainLayout>
           {{
             title: () => '记一笔',
-            icon: () => <Icon name={'left'} />
+            icon: () => <Icon name={'left'} />,
+            default: () => (
+              <>
+                <Tabs
+                  // selected={refKind.value}
+                  // onUpdateSelected={(name: string) => (refKind.value = name)}
+                  v-model:selected={refKind.value}
+                >
+                  <Tab name="支出">icon 列表</Tab>
+                  <Tab name="收入">icon 列表</Tab>
+                </Tabs>
+              </>
+            )
           }}
         </MainLayout>
       </div>
